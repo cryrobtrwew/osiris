a9896c10100299288238e46174797f5edb9a6fc9
FINZ0 <unconfigured@null.spigotmc.org>
2020-02-23 12:56:20 +0100

    fix spaces in name command

--- src/main/java/me/finz0/osiris/command/commands/ClientnameCommand.java
+++ src/main/java/me/finz0/osiris/command/commands/ClientnameCommand.java
@@ -18,9 +18,9 @@ public class ClientnameCommand extends Command {
 
     @Override
     public void onCommand(String command, String[] args) throws Exception {
-        if(!args[0].replace("__", "").equalsIgnoreCase("")) {
-            OsirisMod.MODNAME = args[0].replace("__", "");
-            sendClientMessage("set client name to " + args[0].replace("__", ""));
+        if(!args[0].replace("__", " ").equalsIgnoreCase("")) {
+            OsirisMod.MODNAME = args[0].replace("__", " ");
+            sendClientMessage("set client name to " + args[0].replace("__", " "));
         }else
             sendClientMessage(getSyntax());
     }
