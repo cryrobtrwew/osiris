fed701fd55836b230f027433b2d05dccb90acb19
FINZ0 <unconfigured@null.spigotmc.org>
2020-02-24 16:09:13 +0100

    set display title

--- src/main/java/me/finz0/osiris/command/commands/ClientnameCommand.java
+++ src/main/java/me/finz0/osiris/command/commands/ClientnameCommand.java
@@ -2,6 +2,7 @@ package me.finz0.osiris.command.commands;
 
 import me.finz0.osiris.OsirisMod;
 import me.finz0.osiris.command.Command;
+import org.lwjgl.opengl.Display;
 
 public class ClientnameCommand extends Command {
     @Override
@@ -20,6 +21,7 @@ public class ClientnameCommand extends Command {
     public void onCommand(String command, String[] args) throws Exception {
         if(!args[0].replace("__", " ").equalsIgnoreCase("")) {
             OsirisMod.MODNAME = args[0].replace("__", " ");
+            Display.setTitle(OsirisMod.MODNAME + " " + OsirisMod.MODVER);
             sendClientMessage("set client name to " + args[0].replace("__", " "));
         }else
             sendClientMessage(getSyntax());
