3ef1d5c3363a60cf991975d23a1a125829f3ba13
FINZ0 <unconfigured@null.spigotmc.org>
2020-03-04 16:55:33 +0100

    this should fix rusherhack compatibility

--- src/main/java/me/finz0/osiris/mixin/mixins/MixinMinecraft.java
+++ src/main/java/me/finz0/osiris/mixin/mixins/MixinMinecraft.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
+@Mixin(value = Minecraft.class, priority = 9999)
 public class MixinMinecraft {
 
     @Shadow public EntityPlayerSP player;
